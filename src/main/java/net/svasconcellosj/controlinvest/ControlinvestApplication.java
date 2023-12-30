1345a2a1feda5dc8cb411befb1c93716f8f673ea
Sergio Vasconcellos Junior <svasconcellosj@hotmail.com>
2023-12-29 21:14:27 -0300

    Start inicial do projeto
    -Gerado arquivo inicial do projeto no start.spring.io
    -Feito teste de execução-OK

---
+++ src/main/java/net/svasconcellosj/controlinvest/ControlinvestApplication.java
@@ -0,0 +1,13 @@
+package net.svasconcellosj.controlinvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlinvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlinvestApplication.class, args);
+	}
+
+}
